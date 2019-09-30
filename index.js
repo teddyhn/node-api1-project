2a0847e70f897bb4730fa20047a4d33211aff976
teddyhn <tngo97@gmail.com>
2019-09-30 13:23:50 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+
+const express = require('express'); // CommonJS Modules, equivalent to above
+
+const database = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
