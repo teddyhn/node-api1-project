be0ef42c1f2a07d0f8a5a679d09924a3c3220a04
teddyhn <tngo97@gmail.com>
2019-09-30 15:39:09 -0700

    Rendering users in React app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/users')
+      .then(res => {
+        setUsers(res.data);
+      })
+  }, []);
+
+  console.log(users);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardDeck>
+        {users && users.map(user => (
+          <Card bg="dark" text="white" style={{ width: '18rem' }} key={user.id}>
+            <Card.Body>
+              <Card.Title>{user.name}</Card.Title>
+              <Card.Text>{user.bio}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </CardDeck>
     </div>
   );
 }
