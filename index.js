43dfa9934cfe520350bcdb54b7d27fb2f7233e11
teddyhn <tngo97@gmail.com>
2019-09-30 13:39:21 -0700

    Basic GET and POST endpoints built

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ const database = require('./data/db.js');
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    database
+        .find()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.send(err);
+        });
+});
+
+server.post('/api/users', (req, res) => {
+    const data = req.body;
+
+    console.log('Data: ', data);
+
+    database
+        .insert(data)
+        .then(x => {
+            res.json(x);
+        })
+        .catch(err => {
+            res.json({ errorMessage: "Please provide name and bio for the user." })
+        })
+});
+
+const port = 8080;
+server.listen(port, () => console.log(`\n** API on port ${port} **\n`));
